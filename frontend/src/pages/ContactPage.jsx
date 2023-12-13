95f205f1af07d2f799f880013a0b454dc46ce186
Leo Cedeño Sanchez <leocedsan@campus.monlau.com>
2023-12-13 16:56:09 +0100

    new(componet):navegation

---
+++ frontend/src/pages/ContactPage.jsx
@@ -0,0 +1,8 @@
+export function ContactPage() {
+    return (
+        <div>
+            <h2>Contacto</h2>
+            <p>Si quieres contactar con nosotros puedes hacerlo a través de nuestro correo electrónico:</p>
+            </div>
+    );
+}
---
+++ frontend/src/pages/HelpPage.jsx
@@ -0,0 +1,8 @@
+export function HelpPage() {
+  return (
+    <div>
+      <h2>Help</h2>
+      <p>Si quieres contactar con nosotros puedes hacerlo a través de nuestro correo electrónico:</p>
+    </div>
+  );
+}
