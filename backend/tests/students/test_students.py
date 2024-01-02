a88a9837e0e8b146480d666436236b3ef971de08
Himel Majumder <himelmajumder1@gmail.com>
2024-01-02 05:07:16 +0100

    Update .gitignore and test_students.py

--- backend/tests/students/test_students.py
+++ backend/tests/students/test_students.py
@@ -1,9 +1,10 @@
 from fastapi.testclient import TestClient
-from backend.app.src.students.router import router as students_router
+from fastapi import status
+from app.src.students.router import router as students_router
 
 client = TestClient(students_router)
 
 def test_read_students():
     response = client.get("/students/")
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == []
